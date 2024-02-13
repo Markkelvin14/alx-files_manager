19d2705da4e76e183b56853dc0ddd18696a83483
Markkelvin14 <mjkelvin38@gmail.com>
2024-02-13 11:10:11 +0000

    files_manager

---
+++ utils/redis.js
@@ -0,0 +1,51 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+/**
+ * Represents a Redis client.
+ */
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   */
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  /**
+   * Retrieves the value of a given key.
+   */
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  /**
+   * Stores a key and its value along with an expiration
+   */
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
+
+  /**
+   * Removes the value of a given key.
+   */
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
